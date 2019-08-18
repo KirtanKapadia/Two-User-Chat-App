66435bcc7243565d183c659150632d81ec41a66f
KirtanKapadia <kirtankapadia13@gmail.com>
2019-08-18 17:13:25 +0530

    Layout for Login Activity added

--- app/src/main/java/com/example/android/meandyou/MainActivity.java
+++ app/src/main/java/com/example/android/meandyou/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.android.meandyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    EditText username;
+    EditText password;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+         username = findViewById(R.id.usernameEditText);
+         password = findViewById(R.id.passwordEditText);
+
+        username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username.setCursorVisible(true);
+            }
+        });
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                password.setCursorVisible(true);
+            }
+        });
+
     }
 }
